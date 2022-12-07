6ebc4daf89503ec074f02fa019e7237f90c8ac48
Emma May <ziggit@starhawking.com>
2022-12-06 21:59:02 -0600

    remove unused imports

--- 2022/elixir/day2b.exs
+++ 2022/elixir/day2b.exs
@@ -1,6 +1,3 @@
-
-require Day2b.Score
-require Day2b.Game
 stream = AOC.day(2)
   |> Stream.map(fn x -> Day2b.parse_line(x) end)
   |> Stream.map(fn {opponent, outcome} -> Day2b.Score.points(Day2b.Game.desired(opponent, outcome), outcome) end)
